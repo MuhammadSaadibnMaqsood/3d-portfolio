d89bb2377bdb5fb8889745e9c469240c44c5616d
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-10-01 12:25:46 -0700

    Working on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import Computers from "./canvas/Computers";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="w-full h-screen relative mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
 
-export default Hero
+        <div>
+          <h1 className={`text-white ${styles.heroHeadText}`}>
+            Hi, I'm <span className="text-[#915eff]">Saad</span>{" "}
+          </h1>
+          <p className={`text-white ${styles.heroSubText}`}>
+            Undergraduate Software Engineer skilled in React, Java, and MERN
+            stack, building creative web apps and exploring 3D interactive
+            designs.
+          </p>
+        </div>
+      </div>
+      <Computers />
+    </section>
+  );
+};
+
+export default Hero;
