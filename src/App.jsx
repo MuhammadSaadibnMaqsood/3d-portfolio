00f040a9337b707ef01df2cd13543db0530c987f
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-10-01 11:30:00 -0700

    Working on Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Tech from "./components/Tech";
+import Work from "./components/Works";
+import Feedbacks from "./components/Feedbacks";
+import Contact from './components/Contact'
+import StarsCanvas from './components/StarsCanvas'
 const App = () => {
   return (
-    <div className='text-4xl text-red-500'>App</div>
-  )
-}
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Work />
+      <Feedbacks />
 
-export default App
+      <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { styles } from "../style";
+import { Link } from "react-router-dom";
+import { navLinks } from "../constants/index";
+import { logo, menu, close } from "../assets";
+import { li } from "framer-motion/m";
+// import { link } from "framer-motion/client";
+// import {logo2} from '../assets/'
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`w-full flex items-center py-5 fixed top-0 z-20 bg-primary ${styles.paddingX}`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img
+            src="logo2.png"
+            alt="logo"
+            className="w-9 h-9 object-contain rounded-full"
+          />
+          <p className="text-white text-[14px] font-bold cursor-pointer">
+            Saad<span className="sm:block  hidden">- ibn Maqsood</span>
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white transition-all duration-300 text-[15px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
 
-export default Navbar
+        {/* MOBILE NAV  */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
