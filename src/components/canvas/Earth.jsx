7e68beb286ca43ede025b4d0e1e17f37a6056646
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-10-03 09:15:48 -0700

    Complete #d modeling

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -3,7 +3,10 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "./CanvasLoader";
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF("./planet/scene.gltf");
+  return (
+    <primitive rotation-y={0} object={earth.scene} scale={2.5} position-y={0} />
+  );
 };
 
 const EarthCanvas = () => {
@@ -12,7 +15,12 @@ const EarthCanvas = () => {
       shadows
       frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
