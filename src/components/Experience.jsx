363f55b1f691a70e806afcf97b93f8d9f9440810
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-03-22 03:50:27 -0700

    Fix: Edit experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,8 +4,6 @@ import {
 } from "react-vertical-timeline-component";
 import { motion } from "framer-motion";
 import "react-vertical-timeline-component/style.min.css";
-
-import { styles } from "../style";
 import { experiences } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
